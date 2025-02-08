eef0969d1e701a9cd2d3b8d1e851830d580f60d7
Mitchel <mitchel_jager@hotmail.com>
2025-02-08 14:28:14 +0100

    chore: Include type def

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns a small fingerprint based on the user's browser, used for trend detection.
+ * @param additionalData Optional array of extra data.
+ * @returns An 8-character fingerprint hash.
+ */
+export function getTinyFingerprint(additionalData?: any[]): string;
